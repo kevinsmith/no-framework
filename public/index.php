20ef425ceda1da3abc356221da176d3abb8d194a
Kevin Smith <kevin@kevinsmith.io>
2018-03-11 14:58:55 -0500

    Add DI container.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,21 @@
 <?php
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use ExampleApp\HelloWorld;
+use function DI\create;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$helloWorld = new \ExampleApp\HelloWorld();
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+]);
+
+/** @noinspection PhpUnhandledExceptionInspection */
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
 $helloWorld->announce();
